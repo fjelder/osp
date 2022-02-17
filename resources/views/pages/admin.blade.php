d9162337b48c74f868b352c2275691e43cf377a5
fjelder <42210053+fjelder@users.noreply.github.com>
2022-02-17 20:10:58 +0100

    test payload5

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -3,7 +3,7 @@
 
     <div class="my-10 space-y-3">
         <h2>Zarząd OSP</h2>21php
-        @foreach(DB::select('select * from management2') as $person)
+        @foreach(DB::select('select * from management') as $person)
         <div class="flex items-center justify-start space-x-3">
             <input type="text" value="{{$person->order}}" class="w-10 text-center bg-transparent border-0"
                 disabled="disabled">
